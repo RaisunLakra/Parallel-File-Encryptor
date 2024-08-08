7e06e1b7f1a0bc5fb82574f2f4e06d918785a823
RaisunLakra <raisunlakra18@gmail.com>
2024-08-08 17:01:23 +0530

    created shared memory and implement it

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -3,36 +3,119 @@
 #include <unistd.h>
 #include <cstring>
 #include <sys/wait.h>
+#include <sys/mman.h>
+#include <atomic>
+#include <fcntl.h>
 #include "../encryptDecrypt/Cryption.hpp"
 
-ProcessManagement::ProcessManagement() {}
+ProcessManagement::ProcessManagement()
+{
+    shmFd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shmFd == -1)
+    {
+        perror("shm_open");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(shmFd, sizeof(SharedMemory)) == -1)
+    {
+        perror("ftruncate");
+        exit(EXIT_FAILURE);
+    }
+
+    void *mmap_result = mmap(NULL, sizeof(SharedMemory), PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0);
+    if (mmap_result == MAP_FAILED)
+    {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    sharedMem = static_cast<SharedMemory *>(mmap_result);
+    sharedMem->front = 0;
+    sharedMem->rear = 0;
+    sharedMem->size.store(0);
+}
 
 bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task)
 {
-    taskQueue.push(std::move(task));
+    if (sharedMem->size.load() >= 1000)
+    {
+        return false;
+    }
+
+    strcpy(sharedMem->tasks[sharedMem->rear], task->toString().c_str());
+    sharedMem->rear = (sharedMem->rear + 1) % 1000;
+    sharedMem->size.fetch_add(1);
+
     int pid = fork();
     if (pid < 0)
+    {
         return false;
+    }
     else if (pid > 0)
     {
-        std::cout << "parent process" << std::endl;
+        std::cout << "Parent process" << std::endl;
     }
     else
     {
         std::cout << "Entering child process" << std::endl;
         executeTasks();
         std::cout << "Exiting child process" << std::endl;
+        _exit(0);
     }
     return true;
 }
 
 void ProcessManagement::executeTasks()
 {
-    while (!taskQueue.empty())
+    if (sharedMem->size.load() == 0)
     {
-        std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
-        taskQueue.pop();
-        std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
-        executeCryption(taskToExecute->toString());
+        return;
     }
+
+    char taskStr[256];
+    strcpy(taskStr, sharedMem->tasks[sharedMem->front]);
+    sharedMem->front = (sharedMem->front + 1) % 1000;
+    sharedMem->size.fetch_sub(1);
+
+    executeCryption(taskStr);
 }
+
+// #include <iostream>
+// #include "ProcessManagement.hpp"
+// #include <unistd.h>
+// #include <cstring>
+// #include <sys/wait.h>
+// #include "../encryptDecrypt/Cryption.hpp"
+
+// ProcessManagement::ProcessManagement() {}
+
+// bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task)
+// {
+//     taskQueue.push(std::move(task));
+//     int pid = fork();
+//     if (pid < 0)
+//         return false;
+//     else if (pid > 0)
+//     {
+//         std::cout << "parent process" << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Entering child process" << std::endl;
+//         executeTasks();
+//         std::cout << "Exiting child process" << std::endl;
+//     }
+//     return true;
+// }
+
+// void ProcessManagement::executeTasks()
+// {
+//     while (!taskQueue.empty())
+//     {
+//         std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
+//         taskQueue.pop();
+//         std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
+//         executeCryption(taskToExecute->toString());
+//     }
+// }
--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -1,9 +1,34 @@
+// #ifndef PROCESS_MANAGEMENT_HPP
+// #define PROCESS_MANAGEMENT_HPP
+
+// #include "Task.hpp"
+// #include <queue>
+// #include <memory>
+
+// class ProcessManagement
+// {
+// public:
+//     ProcessManagement();
+//     bool submitToQueue(std::unique_ptr<Task> task);
+//     void executeTasks();
+
+// private:
+//     std::queue<std::unique_ptr<Task>> taskQueue;
+// };
+
+// #endif
+
 #ifndef PROCESS_MANAGEMENT_HPP
 #define PROCESS_MANAGEMENT_HPP
 
 #include "Task.hpp"
 #include <queue>
 #include <memory>
+#include <atomic>
+#include <iostream>
+#include <sys/mman.h> // For shared memory
+#include <fcntl.h>    // For O_* constants
+#include <unistd.h>   // For ftruncate
 
 class ProcessManagement
 {
@@ -13,7 +38,23 @@ public:
     void executeTasks();
 
 private:
-    std::queue<std::unique_ptr<Task>> taskQueue;
+    // std::queue<std::unique_ptr<Task>> taskQueue;
+    struct SharedMemory
+    {
+        std::atomic<int> size;
+        char tasks[1000][256];
+        int front;
+        int rear;
+
+        void printSharedMemory()
+        {
+            std::cout << size.load() << std::endl;
+        }
+    };
+
+    SharedMemory *sharedMem;
+    int shmFd;
+    const char *SHM_NAME = "/my_queue";
 };
 
 #endif
